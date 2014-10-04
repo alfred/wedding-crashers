345fce818f085d649b44db04dc78225758b7f112
matcpn <matcpn@gmail.com>
2014-10-04 15:18:35 -0400

    Added events.js

---
+++ app/models/events.js
@@ -0,0 +1,13 @@
+var eventsSchema = new Schema({
+  name:  String,
+  description: String,
+  date:   Date,
+  url : String,
+  host : String,
+  date: { type: Date, default: Date.now },
+  price : String,
+  location : [{ street : String, 
+              zip : Number,
+              city : String,
+              state : String  }]
+});
